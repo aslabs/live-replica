90661b1faa115144831a2b89adcf13a89e62a82c
Michael Shakarov <michael.shakarov@nex.com>
2018-11-08 12:07:43 +0200

    feat(lit-element-mixin): added formatter support to directive

--- dist/live-replica.js
+++ dist/live-replica.js
@@ -2261,7 +2261,7 @@ module.exports = PatchDiff;
   var root = freeGlobal || freeSelf || Function('return this')();
 
   /** Detect free variable `exports`. */
-  var freeExports = true && exports && !exports.nodeType && exports;
+  var freeExports =  true && exports && !exports.nodeType && exports;
 
   /** Detect free variable `module`. */
   var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && module;
@@ -19824,23 +19824,29 @@ Object.byPath = function(object, path) {
     return object;
 };
 
-function createDirective(replica, property) {
+function setPartValue(part, value, formatter) {    
+    if (formatter) {
+        value = formatter(value);
+    }
+    part.setValue(value);
+}
 
-    const subscribersByPart = new Map();
+function createDirective(replica, property, formatter) {
 
+    const subscribersByPart = new Map();    
     const directive = (part) => {
         // recalling the directive
-        if (subscribersByPart.has(part)) {
-            part.setValue(replica.get(property));
+        if (subscribersByPart.has(part)) {            
+            setPartValue(part, replica.get(property), formatter);
         } else {
-
-            const unsub = replica.subscribe((diff) => {
-                if (diff[property] !== undefined) {
-                    part.setValue(replica.get(property));
+            
+            const unsub = replica.subscribe((diff) => {                
+                if (diff[property] !== undefined) {                                        
+                    setPartValue(part, replica.get(property), formatter);
                     part.commit();
                 }
             });
-
+            
             subscribersByPart.set(part, unsub);
         }
     };
@@ -19856,7 +19862,7 @@ function createDirective(replica, property) {
     return LitElementMixin.directive(directive);
 }
 
-function getDirective(data, path) {
+function getDirective(data, path, formatter) {
 
     if (typeof data !== 'object') {
         throw new Error('live-replica lit-element directive data must be of type object');
@@ -19866,7 +19872,7 @@ function getDirective(data, path) {
 
     if (!replica) {
         const drv = function staticDirective(part) {
-            part.setValue(Object.byPath(data, path) || '');
+            setPartValue(part, Object.byPath(data, path) || '', formatter);
         };
         drv.__litDirective = true;
 
@@ -19892,7 +19898,7 @@ function getDirective(data, path) {
     }
 
     if (!replicasDirectives[property]) {
-        replicasDirectives[property] = createDirective(replica, property);
+        replicasDirectives[property] = createDirective(replica, property, formatter);
     }
 
     return replicasDirectives[property];
