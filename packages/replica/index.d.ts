3189e0e5c0faf64f533af840bc80e94845b6da14
Michael Shakarov <michael.shakarov@nex.com>
2018-11-08 12:08:04 +0200

    updated typings

--- packages/replica/index.d.ts
+++ packages/replica/index.d.ts
@@ -1,11 +1,13 @@
 export declare interface Replica {
-    constructor(remotePath: string, options = {dataObject: {}});
+    constructor(remotePath: string, options?: { dataObject: any });
     subscribeRemote(connection?, connectionCallback?);
-    apply(patch, path: string, options = {});
-    set(fullDocument, path, options = {});
-    splice(patch, path, options = {});
+    apply(patch, path: string, options?);
+    set(fullDocument, path, options?);
+    splice(patch, path, options?);
     unsubscribeRemote();
     destroy();
     getWhenExists(path: string);
     data: ProxyHandler<any>;
+    sync: Promise<any>;
+    existence: Promise<any>;
 }
